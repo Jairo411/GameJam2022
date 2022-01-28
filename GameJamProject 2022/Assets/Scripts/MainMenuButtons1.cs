24c57ef3bd295711c6bd0d8c18c9d77feff0f16c
osamahussein2 <osamahusseinn2@gmail.com>
2022-01-28 18:55:40 -0500

    Added additional scenes to test UI

---
+++ GameJamProject 2022/Assets/Scripts/MainMenuButtons1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons1 : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+	// Start is called before the first frame update
+	void OnMouseUp()
+	{
+		if (isStart)
+		{
+			//Application.LoadLevel(1);
+
+			// or use
+			SceneManager.LoadScene("Scene1");
+		}
+		if (isQuit)
+		{
+			Application.Quit();
+
+			// or use
+			// Debug.Log("QUIT");
+			// SceneManager.LoadScene("Quit")
+		}
+	}
+}
---
+++ GameJamProject 2022/Assets/Scripts/MainMenuButtons2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons2 : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+	// Start is called before the first frame update
+	void OnMouseUp()
+	{
+		if (isStart)
+		{
+			//Application.LoadLevel(4);
+
+			// or use
+			SceneManager.LoadScene("Scene2");
+		}
+		if (isQuit)
+		{
+			Application.Quit();
+
+			// or use
+			// Debug.Log("QUIT");
+			// SceneManager.LoadScene("Quit")
+		}
+	}
+}

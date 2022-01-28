150f76c8e2e8aa4bf7a390afbbae8593976e5021
osamahussein2 <osamahusseinn2@gmail.com>
2022-01-28 18:11:25 -0500

    Created main menu

---
+++ GameJamProject 2022/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+	// Start is called before the first frame update
+	void OnMouseUp()
+	{
+		if (isStart)
+		{
+			Application.LoadLevel(1);
+		}
+		if (isQuit)
+		{
+			Application.Quit();
+		}
+	}
+}

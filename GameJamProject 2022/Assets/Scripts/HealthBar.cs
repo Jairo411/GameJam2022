d787f17b565f82164113e8063ddeba93539fee1d
osamahussein2 <osamahusseinn2@gmail.com>
2022-01-28 22:38:24 -0500

    Added health bar to the player

---
+++ GameJamProject 2022/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient healthGradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = healthGradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+
+        fill.color = healthGradient.Evaluate(slider.normalizedValue);
+    }
+}

aaeb985de5e04e8afa483cd59b29bfc17ca05b5a
osamahussein2 <osamahusseinn2@gmail.com>
2022-01-29 12:50:29 -0500

    Credits added to project

---
+++ GameJamProject 2022/Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsButton : MonoBehaviour
+{
+	public bool isCreditsClicked;
+
+	// Start is called before the first frame update
+	public void OnMouseUp()
+	{
+		if (isCreditsClicked)
+		{
+			SceneManager.LoadScene("Credits"); // Go to credits scene
+		}
+	}
+}
